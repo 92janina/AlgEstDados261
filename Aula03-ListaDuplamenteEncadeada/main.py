d336f92039ec71adb71b4e89784cf698cba07eae
JANINA DE SIQUEIRA <03189344000@senacrs.edu.br>
2026-03-25 11:42:37 -0300

    Aula03ListaDuplamenteEncadeada

---
+++ Aula03-ListaDuplamenteEncadeada/main.py
@@ -0,0 +1,11 @@
+from ListaDuplamenteEncadeada import ListaDuplamente
+
+lista = ListaDuplamente()
+
+lista.imprimir()
+lista.add("João")
+lista.add("Benjamim")
+lista.add("Rafaela")
+lista.add("Júlia")
+
+lista.imprimirReverso()
